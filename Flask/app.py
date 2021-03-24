f787a418005465e697e2b10ab77e64fce7bbc845
Humbert Meyers <humbertmeyers@gmail.com>
2021-03-24 08:09:38 +0100

    add for python requirements

--- Flask/app.py
+++ Flask/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+
 # Page d'acceuil
 @app.route('/')
 def init():
     return render_template("init.html")
+
 
 # Page de connexion
 @app.route('/connexion', methods=['GET', 'POST'])
@@ -16,10 +18,17 @@ def connexion():
     else:
         return render_template('connexion.html')
 
+
 # Page d'une note
 @app.route('/notes/<note>')
 def page_note(note):
-    return "Page de note: %s" % note
+    return f"Page de note: {note}"
+
+
+# Page d'une note
+@app.route('/notes/<note>/share/<person>')
+def page_note(note, person):
+    return f"Votre note {note} à bien été partagée avec {person}"
 
 
 if __name__ == "__main__":
