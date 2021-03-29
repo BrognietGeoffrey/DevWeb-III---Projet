e88765dbf5c1d138d27e865e6eb3aad0d96962b5
BrognietGeoffrey <geoffrey.brogniet@gmail.com>
2021-03-29 21:35:51 +0200

    Update create-db.sql

--- bdd/create-db.sql
+++ bdd/create-db.sql
@@ -1,63 +1,71 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2578
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+//// -- LEVEL 1
+//// -- Tables and References
 
-\f0\fs24 \cf0 CREATE TABLE "users" (\
-  "id" SERIAL PRIMARY KEY,\
-  "last_name" varchar(50),\
-  "sur_name" varchar(50),\
-  "password" varchar(100),\
-  "email" varchar(50),\
-  "notifications" boolean\
-);\
-\
-CREATE TABLE "notes" (\
-  "code" SERIAL PRIMARY KEY,\
-  "name" varchar(50),\
-  "sizeNote" varchar(1000),\
-  "ctag_id" int,\
-  "label_id" int,\
-  "id_group" int,\
-  "creationDate" date\
-);\
-\
-CREATE TABLE "categories" (\
-  "ctag_id" SERIAL,\
-  "ctag_name" varchar(50),\
-  PRIMARY KEY ("ctag_id")\
-);\
-\
-CREATE TABLE "history" (\
-  "code" SERIAL PRIMARY KEY,\
-  "modif" varchar(100),\
-  "date_time" date\
-);\
-\
-CREATE TABLE "labels" (\
-  "label_id" SERIAL,\
-  "label_name" varchar(20),\
-  PRIMARY KEY ("label_id")\
-);\
-\
-CREATE TABLE "groupes" (\
-  "id_group" int,\
-  "id" int,\
-  "nameGroup" varchar(20)\
-);\
-\
-ALTER TABLE "notes" ADD FOREIGN KEY ("ctag_id") REFERENCES "categories" ("ctag_id");\
-\
-ALTER TABLE "history" ADD FOREIGN KEY ("code") REFERENCES "notes" ("code");\
-\
-ALTER TABLE "notes" ADD FOREIGN KEY ("code") REFERENCES "users" ("id");\
-\
-ALTER TABLE "notes" ADD FOREIGN KEY ("label_id") REFERENCES "labels" ("label_id");\
-\
---ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "groupes" ("id");\
-\
---ALTER TABLE "notes" ADD FOREIGN KEY ("id_group") REFERENCES "groupes" ("id_group");\
-\
+// Creating tables
+Table users {
+  id int [pk, increment] // auto-increment
+  last_name varchar(50)
+  sur_name varchar(50)
+  password varchar(100)
+  email varchar(50)
+  notifications boolean
 }
+
+Table notes {
+  code int [pk,increment]
+  name varchar(50)
+  sizeNote varchar(1000)
+  ctag_id int
+  label_id int
+  id_group int
+  creationDate date_time
+ }
+
+Table categories {
+  ctag_id int [pk,increment]
+  ctag_name varchar(50) [pk]
+}
+
+Table history {
+  code int [pk,increment]
+  //name varchar(50) [pk]
+  modif varchar(100)
+  date_time date_time
+}
+
+Table labels {
+  label_id int [pk,increment]
+  label_name varchar(20) [pk]
+}
+
+Table groupes {
+  id_group int [pk, increment]
+  id int [pk]
+  nameGroup varchar(20)
+}
+// Creating references
+// You can also define relaionship separately
+// > many-to-one; < one-to-many; - one-to-one
+Ref: categories.ctag_id - notes.ctag_id
+//Ref: categories.ctag_name - notes.ctag_name
+Ref: notes.code - history.code
+//Ref: notes.name - history.name
+Ref: users.id < notes.code
+Ref : labels.label_id - notes.label_id
+Ref: users.id > groupes.id
+Ref: groupes.id_group - notes.id_group
+//----------------------------------------------//
+
+//// -- LEVEL 2
+//// -- Adding column settings
+
+
+//----------------------------------------------//
+
+//// -- Level 3
+//// -- Enum, Indexes
+
+// Enum for 'products' table below
+
+
+// Indexes: You can define a single or multi-column index
